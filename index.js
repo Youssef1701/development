a47d497fb228d7fd1c238007266e87bd5c54ff6b
Youssef Ehab <yehab1701@icloud.com>
2023-09-10 10:05:23 +0300

    main

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const makeCommit = (n) => {
   const x = random.int(0, 54);
   const y = random.int(0, 6);
   const DATE = moment()
-    .subtract(0, "y")
+    .subtract(1, "y")
     .add(1, "d")
     .add(x, "w")
     .add(y, "d")
@@ -30,4 +30,4 @@ const makeCommit = (n) => {
   });
 };
 
-makeCommit(120);
+makeCommit(220);
